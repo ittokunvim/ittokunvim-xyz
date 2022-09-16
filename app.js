38cdfbdf5eb67d4668d84d89338060005970c814
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-09-17 04:28:16 +0900

    Add error pages

--- app.js
+++ app.js
@@ -24,18 +24,24 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  req.app.get('env') === 'development'
+    ? next(createError(404))
+    : res.render('./404');
 });
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = err;
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render(
+    req.app.get('env') === 'development'
+      ? 'error'
+      : './500', { url: process.env.npm_package_url }
+  );
 });
 
 module.exports = app;
