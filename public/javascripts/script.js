ca469a6e8343321c7bdaba7fe718504ae22b4ea2
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-12-20 00:02:27 +0900

    Updated site footer

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,53 @@
-console.log('script.js');
+window.addEventListener('load', displayFooterTools);
+
+function displayFooterTools() {
+  const toolsData = [
+    {
+      href: 'https://github.com/ittoku-ky73',
+      icon: 'fa-brands fa-github',
+    },
+    {
+      href: "https://twitter.com/ittoku_ky73",
+      icon: "fa-brands fa-twitter"
+    },
+    {
+      href: "https://www.w3schools.com/html/",
+      icon: "fa-brands fa-html5",
+    },
+    {
+      href: "https://www.w3schools.com/css/default.asp",
+      icon: "fa-brands fa-css3-alt",
+    },
+    {
+      href: "https://javascript.info/",
+      icon: "fa-brands fa-js",
+    },
+    {
+      href: "https://nodejs.org/",
+      icon: "fa-brands fa-node",
+    }
+  ];
+
+  const footerTools = document.querySelector('.site__footer__tools');
+
+  const myPara = document.createElement('p');
+  const myStorong = document.createElement('strong');
+  myStorong.textContent = 'web development tools';
+  myPara.appendChild(myStorong);
+
+  const myList = document.createElement('ul');
+
+  toolsData.forEach(tool => {
+    const myLink = document.createElement('a');
+    const myIcon = document.createElement('i');
+
+    myLink.href = tool.href;
+    myIcon.classList = tool.icon;
+
+    myLink.appendChild(myIcon);
+    myList.appendChild(myLink);
+  });
+
+  footerTools.appendChild(myPara);
+  footerTools.appendChild(myList);
+}
