574a2e6b28dec850405ee2eb5404842bace0ec88
いっとく <73953416+ittoku703@users.noreply.github.com>
2023-01-28 23:48:23 +0900

    Remove ittoku tech blog

--- routes/comments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const commentsController = require('../controllers/comments.js');
-
-router.post('/:post_id/comments/', commentsController.create_post);
-router.post('/:post_id/comments/:comment_id', commentsController.delete_post);
-
-module.exports = router;
