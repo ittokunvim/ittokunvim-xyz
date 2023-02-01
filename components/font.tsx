9046ab0600f7468bc28af989d2bb740bb4392526
ittokun <73953416+ittokun@users.noreply.github.com>
2023-02-02 04:22:37 +0900

    Remove SourceHanSans-VF.otf

--- components/font.tsx
+++ components/font.tsx
@@ -1,17 +1,13 @@
 import { Montserrat } from "@next/font/google";
-import localFont from '@next/font/local';
 
 const montserrat = Montserrat({ subsets: ["latin"] });
-const sourceHanSans = localFont({ src: "../styles/SourceHanSans-VF.otf" });
 
 export default function Font() {
-  const fontsFontFamily = `${montserrat.style.fontFamily}, ${sourceHanSans.style.fontFamily}`;
-
   return (
     <style jsx global>
       {`
         html {
-          font-family: ${fontsFontFamily};
+          font-family: ${montserrat.style.fontFamily};
           font-size: 14px;
           word-spacing: 2px;
           letter-spacing: 1px;
