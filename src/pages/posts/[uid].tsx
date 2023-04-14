dee9b4ff80703b6e595ce190f8877d17ccc79d29
ittokun <73953416+ittokun@users.noreply.github.com>
2023-04-15 00:41:23 +0900

    feat: :construction: Add /api/posts/[uid]/update endpoint
    Issue #27

--- src/pages/posts/[uid].tsx
+++ src/pages/posts/[uid].tsx
@@ -9,22 +9,29 @@ import styles from "@/styles/posts/uid.module.css";
 
 export default function PostDetail() {
   const router = useRouter();
+  const { uid } = router.query;
+  const [post, setPost] = useState({
+    id: "", title: "", body: "", created_at: "", updated_at: "",
+  });
+  const [formData, setFormData] = useState({
+    title: "", body: "",
+  });
   const [titleEditing, setTitleEditing] = useState(false);
   const [bodyEditing, setBodyEditing] = useState(false);
   const inputTitle = useRef<HTMLInputElement>(null);
   const divTitle = useRef<HTMLDivElement>(null);
   const textareaBody = useRef<HTMLTextAreaElement>(null);
   const divBody = useRef<HTMLDivElement>(null);
-  const { uid } = router.query;
-  const [post, setPost] = useState({
-    id: "", title: "", body: "", created_at: "", updated_at: "",
-  })
 
   useEffect(() => {
     fetch(`/api/posts/${uid}`)
       .then((res) => res.json())
       .then((data) => {
         setPost(data);
+        setFormData({
+          title: data.title,
+          body: data.body,
+        });
       })
   }, [])
 
@@ -36,8 +43,8 @@ export default function PostDetail() {
       <div className={styles.main}>
         <div className={styles.title}>
           {!titleEditing && <h1 className={styles.headline} ref={divTitle}>{post.title}</h1>}
-          {titleEditing && <form action={`/api/posts/${uid}/update`} method="post">
-            <input type="text" name="title" ref={inputTitle} />
+          {titleEditing && <form onSubmit={handleSubmit}>
+            <input type="text" name="title" ref={inputTitle} onChange={handleChange} />
             <input type="submit" value="Save" />
             <button type="button" onClick={handleTitleEdit}>Cancel</button>
           </form>
@@ -63,8 +70,8 @@ export default function PostDetail() {
             {post.body}
           </div>
           }
-          {bodyEditing && <form action={`/api/posts/${uid}/update`} method="post">
-            <textarea name="body" ref={textareaBody} />
+          {bodyEditing && <form onSubmit={handleSubmit}>
+            <textarea name="body" ref={textareaBody} onChange={handleChange} />
             <div className={styles.article_form_btns}>
               <button type="button" onClick={handleBodyEdit}>Cancel</button>
               <input type="submit" value="Update post" />
@@ -73,20 +80,37 @@ export default function PostDetail() {
           }
         </div>
         <div className={styles.sidebar}>
-          <form action={`/api/posts/${uid}/delete`} method="post">
-            <input type="submit" value="Delete post" onClick={confirmDelete} />
+          <form action={`/api/posts/${uid}/delete`} method="post" onSubmit={confirmDelete}>
+            <input type="submit" value="Delete post" />
           </form>
         </div>
       </div>
     </Layout>
   );
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    fetch(`/api/posts/${uid}/update`, { method: "POST", body: JSON.stringify(formData), })
+      .then((res) => res.json())
+      .then(async (data) => {
+        setTitleEditing(false);
+        setBodyEditing(false);
+        await sleep(10);
+        divTitle.current!.textContent = data.title;
+        divBody.current!.textContent = data.body;
+      })
+    console.log("Form submitted:", formData);
+  }
+
   async function handleTitleEdit() {
     setTitleEditing(!titleEditing);
     if (!titleEditing) {
-      let titleText = divTitle.current!.innerText;
       await sleep(10);
-      inputTitle.current!.value = titleText;
+      inputTitle.current!.value = formData.title;
       inputTitle.current!.focus();
     }
   }
@@ -94,15 +118,14 @@ export default function PostDetail() {
   async function handleBodyEdit() {
     setBodyEditing(!bodyEditing);
     if (!bodyEditing) {
-      let bodyText = divBody.current!.innerText;
       await sleep(10);
-      textareaBody.current!.value = bodyText;
+      textareaBody.current!.value = formData.body;
       textareaBody.current!.focus();
     }
   }
 }
 
-function confirmDelete(e: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+function confirmDelete(e: React.MouseEvent<HTMLFormElement, MouseEvent>) {
   (window.confirm("本当に削除してよろしいですか？"))
     ? "Delete"
     : e.preventDefault();
