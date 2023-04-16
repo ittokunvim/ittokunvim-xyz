d2da2135fc7c4739a999a7a60e9b13dd66ad29fb
ittokun <73953416+ittokun@users.noreply.github.com>
2023-04-17 02:06:16 +0900

    feat: :construction: Add /api/posts/create endpoint
    Issue #27

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -1,25 +1,43 @@
-import Layout from "@/components/layout";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 import styles from "@/styles/posts/create.module.css";
+import Layout from "@/components/layout";
 
 export default function PostCreate() {
+  const router = useRouter();
+  const [form, setForm] = useState({
+    title: "", body: "",
+  })
+
   return (
     <Layout>
       <Head>
         <title>Post Create</title>
       </Head>
       <div className={styles.form}>
-        <form action="/api/posts" method="post">
+        <form onSubmit={handleSubmit}>
           <div className={styles.form_title}>
-            <input type="text" name="title" placeholder="Title" />
+            <input type="text" name="title" placeholder="Title" onChange={handleUpdateFormData} />
           </div>
           <div className={styles.form_body}>
-            <textarea name="body" placeholder="Leave a post" />
+            <textarea name="body" placeholder="Leave a post" onChange={handleUpdateFormData} />
           </div>
           <input type="submit" value="Submit new post" />
         </form>
       </div>
     </Layout>
   );
+
+  function handleUpdateFormData(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    fetch("/api/posts/create", { method: "POST", body: JSON.stringify(form) })
+      .then((res) => res.json())
+      .then((data) => router.push(`/posts/${data.id}`))
+  }
 }
