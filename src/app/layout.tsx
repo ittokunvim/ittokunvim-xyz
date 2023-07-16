994f5a37e0cac4136abfa911d93f68f4c17e67d7
ittokun <73953416+ittokun@users.noreply.github.com>
2023-07-16 21:12:14 +0900

    feat: :heavy_plus_sign: Add Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
-import '@/config/fontawesome'
+import "./globals.css";
+import "@/config/fontawesome";
 
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-import { Noto_Sans_JP } from 'next/font/google'
-const noto_sans_jp = Noto_Sans_JP({ subsets: ['latin'] })
+import { Noto_Sans_JP } from "next/font/google";
+const noto_sans_jp = Noto_Sans_JP({ subsets: ["latin"] });
 
-import Link from 'next/link'
-import Image from 'next/image'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 export const metadata: Metadata = {
-  title: 'ittoku-tech',
-  description: 'Here is ittokunvim portfolio site',
-}
+  title: "ittoku-tech",
+  description: "Here is ittokunvim portfolio site",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const github_url = "https://github.com/ittokunvim";
   const twitter_url = "https://twitter.com/ittokunvim";
@@ -30,32 +30,30 @@ export default function RootLayout({
       <body className={noto_sans_jp.className}>
         <header>
           <Link href="/">
-            <Image
-              src="/logo.svg"
-              width={44}
-              height={44}
-              alt="site logo"
-            />
+            <Image src="/logo.svg" width={44} height={44} alt="site logo" />
           </Link>
           <div>
             <a href={github_url} target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faGithub} size='2xl' style={{color: "#202328"}} />
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="2xl"
+                style={{ color: "#202328" }}
+              />
             </a>
             <a href={twitter_url} target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitter} size='2xl' style={{color: "#1DA1F2" }}/>
+              <FontAwesomeIcon
+                icon={faTwitter}
+                size="2xl"
+                style={{ color: "#1DA1F2" }}
+              />
             </a>
             <a href={zenn_url} target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/zenn.svg"
-                width={32}
-                height={32}
-                alt="zenn logo"
-              />
+              <Image src="/zenn.svg" width={32} height={32} alt="zenn logo" />
             </a>
           </div>
         </header>
         {children}
       </body>
-    </html >
-  )
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,13 @@ export default function Home() {
     <main className={styles.main}>
       <article className={styles.about}>
         <h3>About:</h3>
-        <p>このサイトは<b>ittokunvim</b>のポートフォリオサイトです。ここには自身の活動内容を書いたりしていこうと思っています。</p>
+        <p>
+          このサイトは<b>ittokunvim</b>
+          のポートフォリオサイトです。ここには自身の活動内容を書いたりしていこうと思っています。
+        </p>
         <br />
         <p>まだ何もないけどゆっくりしていってね😄</p>
       </article>
     </main>
-  )
+  );
 }
