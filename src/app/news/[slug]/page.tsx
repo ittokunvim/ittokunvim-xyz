4fec6fee5c7aa303b3aa1f41c954946fa70ca6e5
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-09-23 15:00:25 +0900

    feat: :sparkles: Add news\[slug]\page.module.css
    Issue: #44

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { NewsData, getNewsData } from "../lib";
 
+import styles from "./page.module.css";
+
 type Props = {
   params: { slug: string }
   searchParams: { [key: string]: string | string[] | undefined }
@@ -19,11 +21,15 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug;
   const newsData: NewsData = await getNewsData(slug);
 
+  newsData.date = newsData.date.replace(/-/g, "/") + "に公開";
+
   return (
-    <>
-      <div>{newsData.title}</div>
-      <div>{newsData.date}</div>
-      <div dangerouslySetInnerHTML={{ __html: newsData.contentHtml}} />
-    </>
+    <main className={styles.main}>
+      <article className={styles.news}>
+        <div className={styles.title}>{newsData.title}</div>
+        <div className={styles.date}>{newsData.date}</div>
+        <div className={styles.content_html} dangerouslySetInnerHTML={{ __html: newsData.contentHtml }} />
+      </article>
+    </main>
   );
 }
