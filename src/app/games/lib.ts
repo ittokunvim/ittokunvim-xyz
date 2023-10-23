f0f9abec86e69ebb1683f1c9c54ec344416ca58d
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2023-10-24 03:40:24 +0900

    feat: :heavy_plus_sign: Add thumbnail to GameData
    Issue: #51

--- src/app/games/lib.ts
+++ src/app/games/lib.ts
@@ -4,8 +4,16 @@ export type GameData = {
   slug: string;
   name: string;
   description: string;
+  thumbnail: string;
   width: number;
   height: number;
+};
+
+type GameThumbnail = {
+  src: string;
+  alt: string;
+  width: string;
+  height: string;
 };
 
 export async function getAllGameData(): Promise<GameData[]> {
@@ -28,10 +36,32 @@ export async function getGameData(slug: string): Promise<GameData> {
       slug: "",
       name: "",
       description: "",
+      thumbnail: "",
       width: 0,
       height: 0,
     };
   }
 
   return game;
+}
+
+export async function getGameThumbnail(game: GameData): Promise<GameThumbnail> {
+  const gameSiteURL = process.env.NEXT_PUBLIC_GAMESITE_URL;
+  const { slug, thumbnail } = game;
+  const src = `${gameSiteURL}/${slug}/${thumbnail}`;
+  const alt = `${game.slug} thumbnail`;
+  const [width, height] = splitImageSize(thumbnail);
+
+  return {
+    src,
+    alt,
+    width,
+    height,
+  }
+}
+
+function splitImageSize(image: string): string[] {
+	image = image.split('.').slice(0, -1).join('.') // Remove extension
+
+	return image.split('x'); // split width and height
 }
