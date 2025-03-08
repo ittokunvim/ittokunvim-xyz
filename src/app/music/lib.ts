b73a7956520265ae02bc6ca2ea848d602e10c743
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-03-09 04:03:51 +0900

    feat: :package: Add music/lib.ts
    Issue: #123

---
+++ src/app/music/lib.ts
@@ -0,0 +1,57 @@
+import { formatDate } from "@/app/lib"
+
+const MUSICSITE_URL = process.env.MUSICSITE_URL || "";
+const MUSICSITE_JSON_URL = process.env.MUSICSITE_JSON_URL || "";
+
+type JsonData = {
+  title: string;
+  artist: string;
+  references: string[];
+  createdAt: string;
+};
+
+export type MusicData = {
+  title: string;
+  artist: string;
+  path: string;
+  references: string[];
+  createdAt: string;
+};
+
+async function fetchMusicJson(): Promise<JsonData[]> {
+  try {
+    const response = await fetch(MUSICSITE_JSON_URL, { cache: "force-cache" });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getMusicDataAll(): Promise<MusicData[]> {
+  const musicList = await fetchMusicJson();
+  let musicDataList: MusicData[] = [{
+    title: "",
+    artist: "",
+    path: "",
+    references: [],
+    createdAt: "",
+  }];
+
+  if (musicList === undefined) {
+    return musicDataList;
+  }
+
+  musicDataList = musicList.map((music: JsonData) => {
+    const title = music.title;
+    const artist = music.artist;
+    const path = `${MUSICSITE_URL}/music/${title}.mp3`;
+    const references = music.references;
+    const createdAt = formatDate(music.createdAt);
+
+    return { title, artist, path, references, createdAt, };
+  });
+
+  return musicDataList;
+}
