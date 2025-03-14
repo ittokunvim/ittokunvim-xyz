64de031ddadf3a5109202e965fd5d4bf234f1048
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-03-14 19:59:14 +0900

    feat: :bookmark: Add JsonLd to tool pages
    Issue: #138

--- src/app/tools/hello/page.tsx
+++ src/app/tools/hello/page.tsx
@@ -1,13 +1,50 @@
 import { Metadata } from "next";
-
+import { JsonLd, JsonLdScript } from "@/components/jsonLdScript";
 import styles from "./page.module.css";
 
+const BASE_URL = process.env.BASE_URL || "";
+const SITENAME = process.env.NEXT_PUBLIC_SITENAME || "";
+const title = "Hello, world";
+const description = "This is a hello world page.";
+
 export async function generateMetadata(): Promise<Metadata> {
+  const url = `${BASE_URL}/tools/hello`;
+  const siteName = SITENAME;
+
   return {
-    title: "Hello, world!",
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName,
+      locale: "ja_JP",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      site: "@ittokunvim",
+      creator: "@ittokunvim",
+    },
+    alternates: {
+      canonical: url,
+    },
   };
 }
 
 export default async function Page() {
-  return <h1 className={styles.headline}>Hello, world!</h1>;
+  const jsonLd: JsonLd = {
+    name: title,
+    description,
+  };
+
+  return (
+    <>
+      <h1 className={styles.headline}>{title}</h1>
+      <JsonLdScript data={jsonLd} />
+    </>
+  );
 }
--- src/app/tools/pachi-border/page.tsx
+++ src/app/tools/pachi-border/page.tsx
@@ -1,22 +1,54 @@
 import { Metadata } from "next";
-import styles from "./page.module.css";
 import PachiBorder from "@/components/pachiBorder";
+import { JsonLd, JsonLdScript } from "@/components/jsonLdScript";
+import styles from "./page.module.css";
 
+const BASE_URL = process.env.BASE_URL || "";
+const SITENAME = process.env.NEXT_PUBLIC_SITENAME || "";
 const title = "パチンコボーダー計算ツール";
+const description = "パチンコボーダー計算ツールです";
 
 export async function generateMetadata(): Promise<Metadata> {
+  const url = `${BASE_URL}/tools/pachi-border`;
+  const siteName = SITENAME;
+
   return {
-    title: title,
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName,
+      locale: "ja_JP",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      site: "@ittokunvim",
+      creator: "@ittokunvim",
+    },
+    alternates: {
+      canonical: url,
+    },
   };
 }
 
 export default async function Page() {
+  const jsonLd: JsonLd = {
+    name: title,
+    description,
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.wrap}>
         <h1>{title}</h1>
         <PachiBorder />
       </div>
+      <JsonLdScript data={jsonLd} />
     </main>
   );
 }
