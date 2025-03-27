f821427f2282a3d67264e770a82731c74346c84a
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-03-27 19:09:18 +0900

    feat: :sparkles: Add search form to pictures
    Issue: #151

--- src/components/PictureList/index.tsx
+++ src/components/PictureList/index.tsx
@@ -1,22 +1,48 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-regular-svg-icons";
-import styles from "./style.module.css";
+
 import { PictureData } from "@/lib/picture";
+import SearchForm from "./SearchForm";
+import styles from "./style.module.css";
+
+export type SearchData = {
+  bonus: string;
+  flag: string;
+  album: string;
+};
 
 type Prop = {
   pictures: PictureData[];
   route: string;
 };
 
-export default function PictureList(props: Prop) {
-  const { pictures, route } = props;
+export default function PictureList({ pictures, route }: Prop) {
+  const [pictureList, setPictureList] = useState<PictureData[]>(pictures);
   const { alt, width, height } = {
     alt: "ittokunvim picture",
     width: 200,
     height: 200,
+  };
+  const searchPictures = ({ bonus, flag, album }: SearchData) => {
+    if (bonus === "" && flag === "" && album === "") {
+      setPictureList(pictures);
+    }
+    const bonusRegex = new RegExp(bonus, "i");
+    const flagRegex = new RegExp(flag, "i");
+    const albumRegex = new RegExp(album, "i");
+    const searchPictureList = pictures.filter((picture) => {
+      const isBonusTest = bonusRegex.test(picture.bonus);
+      const isFlagTest = flagRegex.test(picture.flag);
+      const isAlbumTest = albumRegex.test(picture.album);
+      return (isBonusTest && isFlagTest && isAlbumTest);
+    });
+
+    setPictureList(searchPictureList);
   };
 
   return (
@@ -25,8 +51,11 @@ export default function PictureList(props: Prop) {
         <FontAwesomeIcon icon={faImage} />
         写真リスト
       </h3>
+      {route === "/pictures" && (
+        <SearchForm searchPicture={searchPictures} />
+      )}
       <div className={styles.list}>
-        {pictures.map((picture, i) => (
+        {pictureList.map((picture, i) => (
           <div className={styles.item} key={i}>
             <div className={styles.image}>
               <img src={picture.path} alt={alt} width={width} height={height} />
