21a37b2b0e5b75fedd3a041071d19cfb1f0a8e73
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-20 00:20:26 +0900

    feat: :construction: Add convertColorcode components
    Issue: #180

--- src/components/ConvertColorcode/index.tsx
+++ src/components/ConvertColorcode/index.tsx
@@ -1,8 +1,42 @@
+"use client";
+
 import styles from "./styles.module.css";
 
 export default function ConvertColorcode() {
   return (
     <div className={styles.convert_colorcode}>
+      <div className={styles.colorcode}>
+        <input type="text" placeholder="# ffffff" />
+        <input type="submit" value="変換する" />
+      </div>
+      <div className={styles.rgb}>
+        <input type="text" placeholder="R 255" />
+        <input type="text" placeholder="G 255" />
+        <input type="text" placeholder="B 255" />
+        <input type="submit" value="変換する" />
+      </div>
+      <div className={styles.output}>
+        <table>
+          <tbody>
+            <tr>
+              <th>16進数</th>
+              <td colSpan={3}>ffffff</td>
+            </tr>
+            <tr>
+              <th>RGB</th>
+              <td>255</td>
+              <td>255</td>
+              <td>255</td>
+            </tr>
+            <tr>
+              <th>RGB パーセント</th>
+              <td>1.0</td>
+              <td>1.0</td>
+              <td>1.0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
