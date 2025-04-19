3f40dcfa3dc768aceb253058e4516062264c4009
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-19 23:50:43 +0900

    feat: :construction: Add tools/convert-colorcode
    Issue: #180

---
+++ src/app/tools/convert-colorcode/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next";
+import { getToolData } from "@/lib/tools";
+import { MetadataProps, setMetadata } from "@/lib/utils";
+import ConvertColorcode from "@/components/ConvertColorcode";
+import { JsonLd, JsonLdScript } from "@/components/JsonLdScript";
+import styles from "./page.module.css";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
+const slug = "convert-colorcode";
+const tool = getToolData(slug);
+const title = tool.name;
+const description = tool.description;
+const route = "/tools/" + slug;
+const url = BASE_URL + route;
+const metadataProps: MetadataProps = {
+  title,
+  description,
+  url,
+};
+
+export const metadata: Metadata = setMetadata(metadataProps);
+
+export default async function Page() {
+  const jsonLd: JsonLd = {
+    name: title,
+    description,
+  };
+
+  return (
+    <main className={styles.main}>
+      <ConvertColorcode />
+      <JsonLdScript data={jsonLd} />
+    </main>
+  );
+}
---
+++ src/components/ConvertColorcode/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./styles.module.css";
+
+export default function ConvertColorcode() {
+  return (
+    <div className={styles.convert_colorcode}>
+    </div>
+  );
+}
