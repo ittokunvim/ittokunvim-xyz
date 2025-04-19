61ceeb2c7692a4d18ca793a332ff664a1b33aefd
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-20 02:06:06 +0900

    fix: :construction_worker: Fixed form value
    Issue: #180

--- src/components/ConvertColorcode/index.tsx
+++ src/components/ConvertColorcode/index.tsx
@@ -1,19 +1,81 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 export default function ConvertColorcode() {
+  const [inputColorcodeValue, setInputColorcodeValue] = useState<string>("");
+  const [inputRedValue, setInputRedValue] = useState<string>("");
+  const [inputGreenValue, setInputGreenValue] = useState<string>("");
+  const [inputBlueValue, setInputBlueValue] = useState<string>("");
+  const handleInputColorcodeChange = (value: string) => {
+    const regex = /^[A-Fa-f0-9]{1,6}$/;
+
+    value = varidateInput(regex, value);
+    setInputColorcodeValue(value);
+  };
+  const handleInputRedChange = (value: string) => {
+    const regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$/;
+
+    value = varidateInput(regex, value);
+    setInputRedValue(value);
+  };
+  const handleInputGreenChange = (value: string) => {
+    const regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$/;
+
+    value = varidateInput(regex, value);
+    setInputGreenValue(value);
+  };
+  const handleInputBlueChange = (value: string) => {
+    const regex = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$/;
+
+    value = varidateInput(regex, value);
+    setInputBlueValue(value);
+  };
+  const handleInputColorcodeSubmit = () => {
+  };
+  const handleInputRgbSubmit = () => {
+  };
+
   return (
     <div className={styles.convert_colorcode}>
       <div className={styles.colorcode}>
-        <input type="text" placeholder="# ffffff" />
-        <input type="submit" value="変換する" />
+        <input
+          type="text"
+          placeholder="# ffffff"
+          value={inputColorcodeValue}
+          onChange={(e) => handleInputColorcodeChange(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="変換する"
+          onClick={handleInputColorcodeSubmit}
+        />
       </div>
       <div className={styles.rgb}>
-        <input type="text" placeholder="R 255" />
-        <input type="text" placeholder="G 255" />
-        <input type="text" placeholder="B 255" />
-        <input type="submit" value="変換する" />
+        <input
+          type="text"
+          placeholder="R 255"
+          value={inputRedValue}
+          onChange={(e) => handleInputRedChange(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="G 255"
+          value={inputGreenValue}
+          onChange={(e) => handleInputGreenChange(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="B 255"
+          value={inputBlueValue}
+          onChange={(e) => handleInputBlueChange(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="変換する"
+          onClick={handleInputRgbSubmit}
+        />
       </div>
       <div className={styles.output}>
         <table>
@@ -39,4 +101,11 @@ export default function ConvertColorcode() {
       </div>
     </div>
   );
+}
+
+function varidateInput(regex: RegExp, value: string): string {
+  if (!regex.test(value)) {
+    return value.slice(0, -1);
+  }
+  return value;
 }
