04cf517f400d182fc28259f4c74a4f625216ebce
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-24 00:29:12 +0900

    fix: :card_file_box: Add published attr to doc JsonData
    Issue: #184

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -15,6 +15,7 @@ type JsonData = {
   title: string;
   description: string,
   path: string;
+  published: boolean,
   createdAt: string;
   updatedAt: string;
 };
@@ -35,17 +36,21 @@ export type DocContentData = {
   updatedAt: string;
 };
 
+// 外部の記事サイトのJSONデータを取得して解析する
 async function fetchDocsJson(): Promise<JsonData[]> {
   try {
     const response = await fetch(DOCSSITE_JSON_URL, { cache: "force-cache" });
-    const data = await response.json();
-    return data;
+    const data: JsonData[] = await response.json();
+
+    // 公開するデータのみをフィルタリングして返す
+    return data.filter(data => data.published);
   } catch (error) {
     console.error(error);
     return [];
   }
 }
 
+// 全てのドキュメントデータを取得・整形する
 export async function getDocDataAll(): Promise<DocData[]> {
   const docs = await fetchDocsJson();
   let docDataList: DocData[] = [{
@@ -56,10 +61,13 @@ export async function getDocDataAll(): Promise<DocData[]> {
     updatedAt: "",
   }];
 
+  // データが取得できない場合、デフォルト値を返す
   if (docs === undefined) {
     return docDataList;
   }
 
+  // 記事を更新日時を基準にソート
+  // 同じ更新日時の場合は作成日時を基準にソート
   docs.sort((a: JsonData, b: JsonData) => {
     if (a.updatedAt === b.updatedAt) {
       return a.createdAt < b.createdAt ? 1 : -1;
@@ -67,6 +75,8 @@ export async function getDocDataAll(): Promise<DocData[]> {
       return a.updatedAt < b.updatedAt ? 1 : -1;
     }
   });
+
+  // データを整形して`DocData`配列を作成
   docDataList = docs.map((doc: JsonData) => {
     const href = `/docs/${doc.slug}`;
     const title = doc.title;
@@ -80,11 +90,13 @@ export async function getDocDataAll(): Promise<DocData[]> {
    return docDataList;
 }
 
+// 全ての記事の`slug`の配列を取得
 export async function getDocSlugAll(): Promise<string[]> {
   const docs = await fetchDocsJson();
   return docs.map((doc: JsonData) => doc.slug);
 }
 
+// 指定した`slug`に対応する記事を取得
 export async function getDocData(slug: string): Promise<DocContentData> {
   const docs = await fetchDocsJson();
   const doc = docs.find((doc: JsonData) => doc.slug === slug);
@@ -96,6 +108,7 @@ export async function getDocData(slug: string): Promise<DocContentData> {
     updatedAt: "",
   };
 
+  // データが取得できない場合、デフォルト値を返す
   if (doc === undefined) {
     return docData;
   }
@@ -106,6 +119,7 @@ export async function getDocData(slug: string): Promise<DocContentData> {
   const createdAt = formatDate(doc.createdAt);
   const updatedAt = formatDate(doc.updatedAt);
 
+  // HTMLコンテンツが空の場合、デフォルト値を返す
   if (contentHtml === "") {
     return docData;
   }
@@ -113,6 +127,7 @@ export async function getDocData(slug: string): Promise<DocContentData> {
   return { title, description, contentHtml, createdAt, updatedAt, };
 }
 
+// 指定された`path`の記事のHTMLコンテンツを取得し整形する
 async function getDocContentHtml(path: string): Promise<string> {
   const absoluteUrl = new URL(path, DOCSSITE_URL);
   const content = await fetch(absoluteUrl.href, { cache: "force-cache" })
@@ -138,6 +153,7 @@ async function getDocContentHtml(path: string): Promise<string> {
   return contentHtml;
 }
 
+// 相対URLを絶対URLに変換する関数
 function replaceRelativeUrlToAbsoluteUrl(path: string, content: string): string {
   const splitPath = path.split("/");
   const excludeFilenamePath = splitPath.slice(0, splitPath.length - 1).join("/");
@@ -146,6 +162,7 @@ function replaceRelativeUrlToAbsoluteUrl(path: string, content: string): string 
   const relativeImageRegex = /!?\[[^\]]+\]\((?!https|ftp:\/\/)[^\)]+\)/g;
   const imageUrlRegex = /\]\(([^)]+)\)/;
 
+  // 相対画像リンクを絶対URLに変換
   content.match(relativeImageRegex)?.forEach((relativeImage) => {
     if (imageUrlRegex.test(relativeImage)) {
       const url = imageUrlRegex.exec(relativeImage)![1];
@@ -156,6 +173,7 @@ function replaceRelativeUrlToAbsoluteUrl(path: string, content: string): string 
   const relativeImageTagRegex = /<img src="(?!https|ftp:\/\/)[^"]+"/g;
   const imageSrcRegex = /src="([^"]+)"/;
 
+  // <img>タグの相対src属性を絶対URLに変換
   content.match(relativeImageTagRegex)?.forEach((relativeImageTag) => {
     if (imageSrcRegex.test(relativeImageTag)) {
       const src = imageSrcRegex.exec(relativeImageTag)![1];
@@ -166,10 +184,14 @@ function replaceRelativeUrlToAbsoluteUrl(path: string, content: string): string 
   return content;
 }
 
+// コードブロックにタイトル属性を追加する関数
+//
+// 例：javascript:filename.jsを検出し変換する
 function replaceCodeBlockTitle(content: string): string {
   const codeBlockTitleRegex = /```[a-zA-Z]+:[\w\.]+/g;
   const filenameRegex = /:[\w\.]+/;
 
+  // コードブロックのタイトル部分を変換して、`title`属性を追加
   content.match(codeBlockTitleRegex)?.forEach((codeBlock) => {
     const filename = filenameRegex.exec(codeBlock)![0];
     content = content.replace(filename, ` title="${filename.slice(1)}"`);
