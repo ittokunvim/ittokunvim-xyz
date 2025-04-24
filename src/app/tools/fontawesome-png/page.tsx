27d6cd59aff120135f6e9aa8694adf6ca8fff1ef
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-24 19:24:01 +0900

    feat: :construction: Add /tools/fontawesome-png page
    Issue: #181

---
+++ src/app/tools/fontawesome-png/page.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+import { getToolData } from "@/lib/tools";
+import { MetadataProps, setMetadata } from "@/lib/utils";
+import { JsonLd, JsonLdScript } from "@/components/JsonLdScript";
+import styles from "./page.module.css";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
+const slug = "fontawesome-png";
+const tool = getToolData(slug);
+const title = tool.name;
+const description = tool.description;
+const route = "/tools/" + slug;
+const url = BASE_URL + route;
+const metadataProps: MetadataProps = {
+  title,
+  description,
+  url,
+};
+
+export const metadata: Metadata = setMetadata(metadataProps);
+
+export default async function Page() {
+  const jsonLd: JsonLd = {
+    name: title,
+    description,
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.title}>
+        <h2>{title}</h2>
+      </div>
+      <JsonLdScript data={jsonLd} />
+    </main>
+  );
+}
