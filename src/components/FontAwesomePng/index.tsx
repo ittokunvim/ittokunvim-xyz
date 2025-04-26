337b60b331002ef23b0a002ea43cf280cc905095
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-26 15:33:52 +0900

    fix: :children_crossing: Add Icon option to FontAwesomePng
    Issue: #181

--- src/components/FontAwesomePng/index.tsx
+++ src/components/FontAwesomePng/index.tsx
@@ -21,6 +21,10 @@ export default function FontAwesomePng() {
   const [iconList, setIconList] = useState<IconDefinition[]>([]);
   const [iconData, setIconData] = useState<IconData>(defaultIconData);
   const [inputIconValue, setInputIconValue] = useState<string>("");
+  const [iconOptionWidth, setIconOptionWidth] = useState<number>(256);
+  const [iconOptionHeight, setIconOptionHeight] = useState<number>(256);
+  const [iconOptionColor, setIconOptionColor] = useState<string>("#000000");
+  const [iconOptionBgColor, setIconOptionBgColor] = useState<string>("#ffffff");
   const svgRef = useRef<SVGSVGElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   // フォームの値が変更された時の処理
@@ -75,9 +79,29 @@ export default function FontAwesomePng() {
       return;
     }
 
+    // Svgのスタイルを設定
+    svg.style.color = iconOptionColor;
+    svg.style.backgroundColor = iconOptionBgColor;
+
+    // Canvasのサイズを設定
+    canvas.width = iconOptionWidth;
+    canvas.height = iconOptionHeight;
+
     // CanvasにSVGを描画する
     renderSvgToCanvas(svg, canvas);
   }, [iconData]);
+  const handleInputOptionWidth = (value: string) => {
+    setIconOptionWidth(Number(value));
+  };
+  const handleInputOptionHeight = (value: string) => {
+    setIconOptionHeight(Number(value));
+  };
+  const handleInputOptionColor = (value: string) => {
+    setIconOptionColor(value);
+  };
+  const handleInputOptionBgColor = (value: string) => {
+    setIconOptionBgColor(value);
+  };
 
   return (
     <div className={styles.fontawesome_png}>
@@ -110,7 +134,13 @@ export default function FontAwesomePng() {
       <div className={styles.canvas}>
         <canvas ref={canvasRef}></canvas>
       </div>
-      <div className={styles.select}>
+      <div
+        className={styles.preview}
+        style={{
+          width: `${iconOptionWidth}px`,
+          height: `${iconOptionHeight}px`,
+        }}
+      >
         <svg
           ref={svgRef}
           aria-hidden={true}
@@ -121,12 +151,52 @@ export default function FontAwesomePng() {
           role="img"
           xmlns="http://www.w3.org/2000/svg"
           viewBox={`0 0 ${iconData.width} ${iconData.height}`}
+          style={{
+            color: iconOptionColor,
+            backgroundColor: iconOptionBgColor,
+          }}
         >
           <path
             fill="currentColor"
             d={`${iconData.data}`}
           ></path>
         </svg>
+      </div>
+      <div className={styles.option}>
+        <div>
+          <label>幅：</label>
+          <input
+            type="number"
+            value={iconOptionWidth}
+            onChange={(e) => handleInputOptionWidth(e.target.value)}
+          />
+          <span>px</span>
+        </div>
+        <div>
+          <label>高さ：</label>
+          <input
+            type="number"
+            value={iconOptionHeight}
+            onChange={(e) => handleInputOptionHeight(e.target.value)}
+          />
+          <span>px</span>
+        </div>
+        <div>
+          <label>アイコンの色：</label>
+          <input
+            type="color"
+            value={iconOptionColor}
+            onChange={(e) => handleInputOptionColor(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>背景色：</label>
+          <input
+            type="color"
+            value={iconOptionBgColor}
+            onChange={(e) => handleInputOptionBgColor(e.target.value)}
+          />
+        </div>
       </div>
     </div>
   );
@@ -145,9 +215,6 @@ function renderSvgToCanvas(svg: SVGSVGElement, canvas: HTMLCanvasElement) {
   const svgData = new XMLSerializer().serializeToString(svg);
   const svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
   const svgUrl = URL.createObjectURL(svgBlob);
-
-  // Canvasのサイズを設定
-  canvas.width = canvas.height = 256;
 
   // Canvasの描画コンテキストを取得
   const ctx = canvas.getContext("2d");
